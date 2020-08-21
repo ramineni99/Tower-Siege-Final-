fc23f852d71beb661eabfd69a61a26d3e94b761c
ramineni99 <67611918+ramineni99@users.noreply.github.com>
2020-08-20 20:34:11 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,68 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var world;
+
+const Constraint = Matter.Constraint;
+
+function setup() {
+  
+
+  engine = Engine.create();
+  world = engine.world
+  createCanvas(800,400);
+  ground = new Ground(600,380,300,10);
+  ground2 = new Ground(600,150,300,10);
+  block1 = new Block(600,350,50,50);
+  block2 = new Block(550,350,50,50);
+  block3 = new Block(650,350,50,50);
+  block4 = new Block(700,350,50,50);
+  block5 = new Block(500,350,50,50);
+  block6 = new Block(600,350,50,50);
+  block7 = new Block(600,350,50,50);
+  block8 = new Block(650,340,50,50);
+  block9 = new Block(550,350,50,50);
+  block10 = new Block(650,120,50,50);
+  block11 = new Block(600,120,50,50);
+  block12 = new Block(550,120,50,50);
+  block13 = new Block(600,80,50,50);
+
+  polygon = new Polygon(300,300,80);
+  slingshot = new Slingshot(polygon.body,{x:300,y:200});
+
+  ground.display();
+  Engine.run(engine);
+  
+}
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  Engine.update(engine);
+  ground.display();
+  ground2.display();
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+  block7.display();
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+  block13.display();
+
+  polygon.display();
+   drawSprites();
+}
+function mouseDragged()
+{
+Matter.Body.setPosition(polygon.body,{x:mouseX,y:mouseY})
+}
+function mouseReleased(){
+slingshot.fly();
+}
